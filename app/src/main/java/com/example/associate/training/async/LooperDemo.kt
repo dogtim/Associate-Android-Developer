fe69537e7bce245eaf87894cdd4e9d3f13968d9f
TimChen <fatfattim@gmail.com>
2022-07-02 18:10:35 +0800

    Create LooperDemo for handling looper and work thread

---
+++ app/src/main/java/com/example/associate/training/async/LooperDemo.kt
@@ -0,0 +1,60 @@
+package com.example.associate.training.async
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+
+class LooperDemo {
+
+    lateinit var mLooperThread: LooperThread
+
+    fun onCreate() {
+        mLooperThread =
+            LooperThread() // Start the worker thread, so that it is ready to process messages.
+        mLooperThread.start();
+    }
+
+    fun onClick() {
+        // There is race condition between the setup of mHandler on a background thread and this usage on the UI thread. Hence, validate that mHandler is available.
+        if (mLooperThread.mHandler != null) {
+
+            //Initialize a Message-object with the what argument arbitrarily set to 0.
+            val msg = mLooperThread.mHandler.obtainMessage(0)
+            mLooperThread.mHandler.sendMessage(msg)
+        }
+    }
+
+    fun doLongRunningOperation() {
+        // Add long running operation here.
+    }
+
+    fun onDestroy() {
+        mLooperThread.mHandler.getLooper().quit()
+        // Terminate the background thread. The call to Looper.quit() stops the dispatching of messages and releases Looper.loop()
+        // from blocking so the run method can finish, leading to the termination of the thread.
+    }
+}
+
+class LooperThread : Thread() {
+
+    public lateinit var mHandler: Handler
+
+    override fun run() {
+        Looper.prepare() // Associate a Looper—and implicitly a MessageQueue—with the thread.
+        mHandler = object : Handler(Looper.getMainLooper()) {
+            // Set up a Handler to be used by the producer for inserting messages in the queue.
+            // Here we use the default constructor so it will bind to the Looper of the current thread.
+            // Hence, this Handler can created only after Looper.prepare(), or it will have nothing to bind to.
+            override fun handleMessage(msg: Message) {
+                // Callback that runs when the message has been dispatched to the worker thread.
+                // It checks the what parameter and then executes the long operation.
+                if (msg.what == 0) {
+                    //doLongRunningOperation();
+                }
+            }
+        }
+
+        Looper.loop()
+        // Start dispatching messages from the message queue to the consumer thread. This is a blocking call, so the worker thread will not finish.
+    }
+}
