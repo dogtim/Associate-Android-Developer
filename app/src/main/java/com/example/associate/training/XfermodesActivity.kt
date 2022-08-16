4a7593f7d49c96bb7efab42e2bf1213d55c2d20b
HsiaoTing Chen <fatfattim@gmail.com>
2022-08-16 15:42:14 +0800

    Rename & remove the warnings

--- app/src/main/java/com/example/associate/training/XfermodesActivity.kt
+++ app/src/main/java/com/example/associate/training/XfermodesActivity.kt
@@ -15,17 +15,19 @@ class XfermodesActivity : AppCompatActivity() {
     }
 
     private class SampleView(context: Context?) : View(context) {
-        private val mSrcB: Bitmap
-        private val mDstB: Bitmap
-        private val mBG // background checker-board pattern
-                : Shader
+        private val srcBitmap: Bitmap
+        private val dstBitmap: Bitmap
+        // background checker-board pattern
+        private val shader: Shader
+        private val labelP = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val paint = Paint()
 
         // create a bitmap with a circle, used for the "dst" image
         fun makeDst(w: Int, h: Int): Bitmap {
             val bm = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
             val c = Canvas(bm)
             val p = Paint(Paint.ANTI_ALIAS_FLAG)
-            p.setColor(-0x33bc)
+            p.color = -0x33bc
             c.drawOval(RectF(0F, 0F, (w * 3 / 4).toFloat(), (h * 3 / 4).toFloat()), p)
             return bm
         }
@@ -35,7 +37,7 @@ class XfermodesActivity : AppCompatActivity() {
             val bm = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
             val c = Canvas(bm)
             val p = Paint(Paint.ANTI_ALIAS_FLAG)
-            p.setColor(-0x995501)
+            p.color = -0x995501
             c.drawRect(
                 (w / 3).toFloat(), (h / 3).toFloat(), (w * 19 / 20).toFloat(),
                 (h * 19 / 20).toFloat(), p
@@ -45,37 +47,35 @@ class XfermodesActivity : AppCompatActivity() {
 
         override fun onDraw(canvas: Canvas) {
             canvas.drawColor(Color.WHITE)
-            val labelP = Paint(Paint.ANTI_ALIAS_FLAG)
-            labelP.setTextAlign(Paint.Align.CENTER)
-            val paint = Paint()
-            paint.setFilterBitmap(false)
+            labelP.textAlign = Paint.Align.CENTER
+            paint.isFilterBitmap = false
             canvas.translate(15F, 35F)
             var x = 0f
             var y = 0f
             for (i in sModes.indices) {
                 // draw the border
-                paint.setStyle(Paint.Style.STROKE)
-                paint.setShader(null)
+                paint.style = Paint.Style.STROKE
+                paint.shader = null
                 canvas.drawRect(
                     x - 0.5f, y - 0.5f,
                     x + W + 0.5f, y + H + 0.5f, paint
                 )
                 // draw the checker-board pattern
-                paint.setStyle(Paint.Style.FILL)
-                paint.setShader(mBG)
+                paint.style = Paint.Style.FILL
+                paint.shader = shader
                 canvas.drawRect(x, y, x + W, y + H, paint)
                 // draw the src/dst example into our offscreen bitmap
                 val sc: Int = canvas.saveLayer(x, y, x + W, y + H, null)
                 canvas.translate(x, y)
-                canvas.drawBitmap(mDstB, 0f, 0f, paint)
-                paint.setXfermode(sModes[i])
-                canvas.drawBitmap(mSrcB, 0f, 0f, paint)
-                paint.setXfermode(null)
+                canvas.drawBitmap(dstBitmap, 0f, 0f, paint)
+                paint.xfermode = sModes[i]
+                canvas.drawBitmap(srcBitmap, 0f, 0f, paint)
+                paint.xfermode = null
                 canvas.restoreToCount(sc)
                 // draw the label
                 canvas.drawText(
                     sLabels[i],
-                    x + W / 2, y - labelP.getTextSize() / 2, labelP
+                    x + W / 2, y - labelP.textSize / 2, labelP
                 )
                 x += W + 10
                 // wrap around when we've drawn enough for one row
@@ -108,6 +108,7 @@ class XfermodesActivity : AppCompatActivity() {
                 PorterDuffXfermode(PorterDuff.Mode.MULTIPLY),
                 PorterDuffXfermode(PorterDuff.Mode.SCREEN)
             )
+
             private val sLabels = arrayOf(
                 "Clear", "Src", "Dst", "SrcOver",
                 "DstOver", "SrcIn", "DstIn", "SrcOut",
@@ -117,8 +118,8 @@ class XfermodesActivity : AppCompatActivity() {
         }
 
         init {
-            mSrcB = makeSrc(W, H)
-            mDstB = makeDst(W, H)
+            srcBitmap = makeSrc(W, H)
+            dstBitmap = makeDst(W, H)
             // make a ckeckerboard pattern
             val bm = Bitmap.createBitmap(
                 intArrayOf(
@@ -127,14 +128,14 @@ class XfermodesActivity : AppCompatActivity() {
                 ), 2, 2,
                 Bitmap.Config.RGB_565
             )
-            mBG = BitmapShader(
+            shader = BitmapShader(
                 bm,
                 Shader.TileMode.REPEAT,
                 Shader.TileMode.REPEAT
             )
             val m = Matrix()
             m.setScale(6F, 6F)
-            mBG.setLocalMatrix(m)
+            shader.setLocalMatrix(m)
         }
     }
 }
