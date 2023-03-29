681b0a04856bc87c3eaf189c53299b765b4029f1
HsiaoTing Chen <fatfattim@gmail.com>
2023-03-29 17:00:34 +0800

    Give State for showing the loading progress indicator

--- app/src/main/java/com/example/associate/training/animation/AnimationActivity.kt
+++ app/src/main/java/com/example/associate/training/animation/AnimationActivity.kt
@@ -7,12 +7,17 @@ import android.graphics.*
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.example.associate.training.R
+import com.example.associate.training.databinding.ActivityAnimationBinding
+import com.example.associate.training.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
@@ -22,15 +27,37 @@ import java.util.*
 class AnimationActivity : AppCompatActivity(), ContextCallback {
 
     private val viewModel: AnimationViewModel by viewModels()
+    private lateinit var binding: ActivityAnimationBinding
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animation)
-        viewModel.setCallback(this)
+        binding = ActivityAnimationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViewModel()
         setListener()
         startAnimation()
         grantPermission()
+    }
+
+    private fun setupViewModel() {
+        viewModel.setCallback(this)
+        viewModel.state.observe(this) { state ->
+            when (state) {
+                is EncodingState.Init -> showToast("Init")
+                is EncodingState.Loading -> showLoading()
+                is EncodingState.Complete -> showToast("Complete")
+            }
+        }
+    }
+
+    private fun showLoading() {
+        binding.loadingProgress.isIndeterminate = true
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        binding.loadingProgress.isIndeterminate = false
     }
 
     // Grant permission to write to external storage
@@ -44,19 +71,13 @@ class AnimationActivity : AppCompatActivity(), ContextCallback {
     }
 
     private fun setListener() {
-        findViewById<AppCompatButton>(R.id.export_to_video).setOnClickListener {
-            CoroutineScope(IO).launch {
-                try {
-                    viewModel.startEncoding()
-                } catch (e: Exception) {
-                    // Handle exception here
-                }
-            }
+        binding.exportToVideo.setOnClickListener {
+            viewModel.startEncoding()
         }
     }
 
     private fun startAnimation() {
-        val imageView: ImageView = findViewById(R.id.animation_img)
+        val imageView: ImageView = binding.animationImg
 
         val zoomIn = ObjectAnimator.ofPropertyValuesHolder(
             imageView,
--- app/src/main/java/com/example/associate/training/animation/AnimationViewModel.kt
+++ app/src/main/java/com/example/associate/training/animation/AnimationViewModel.kt
@@ -7,8 +7,13 @@ import android.media.MediaFormat
 import android.media.MediaMuxer
 import android.util.Log
 import android.view.Surface
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.associate.training.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 interface ContextCallback {
@@ -16,10 +21,17 @@ interface ContextCallback {
     fun getFile(): File
 }
 
+sealed class EncodingState {
+    object Init : EncodingState()
+    object Loading : EncodingState()
+    object Complete : EncodingState()
+}
 class AnimationViewModel : ViewModel() {
-
+    private val TAG = this::class.simpleName
     private var videoTrackIndex: Int = 0
     private var callback: ContextCallback? = null
+    private val _state = MutableLiveData<EncodingState>()
+    val state: LiveData<EncodingState> = _state
 
     fun setCallback(callback: ContextCallback) {
         this.callback = callback
@@ -27,32 +39,42 @@ class AnimationViewModel : ViewModel() {
 
     // Encode the zoomIn animation for 5 durations
     fun startEncoding() {
-        val size = 720
-        val bitrate = 3000000
-        val mime = "video/avc"
-        val format = MediaFormat.createVideoFormat(mime, size, size)
-        format.setInteger(MediaFormat.KEY_BIT_RATE, bitrate)
-        format.setInteger(MediaFormat.KEY_FRAME_RATE, 30)
-        format.setInteger(
-            MediaFormat.KEY_COLOR_FORMAT,
-            MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
-        )
-        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1)
-        val codec = MediaCodec.createEncoderByType(mime)
-        codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
-        val surface: Surface = codec.createInputSurface()
-        codec.start()
-        // Create MediaMuxer and add track
-        val VIDEO_PATH = createTempFile()
-        val muxer = MediaMuxer(VIDEO_PATH.path, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
-        val bufferInfo = MediaCodec.BufferInfo()
+        if (_state.value == EncodingState.Loading) return
+        _state.postValue(EncodingState.Loading)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val size = 720
+                val bitrate = 3000000
+                val mime = "video/avc"
+                val format = MediaFormat.createVideoFormat(mime, size, size)
+                format.setInteger(MediaFormat.KEY_BIT_RATE, bitrate)
+                format.setInteger(MediaFormat.KEY_FRAME_RATE, 30)
+                format.setInteger(
+                    MediaFormat.KEY_COLOR_FORMAT,
+                    MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
+                )
+                format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1)
+                val codec = MediaCodec.createEncoderByType(mime)
+                codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+                val surface: Surface = codec.createInputSurface()
+                codec.start()
+                // Create MediaMuxer and add track
+                val VIDEO_PATH = createTempFile()
+                val muxer = MediaMuxer(VIDEO_PATH.path, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+                val bufferInfo = MediaCodec.BufferInfo()
 
-        encode(surface, bufferInfo, codec, muxer, size)
-        // Release resources
-        codec.stop()
-        codec.release()
-        muxer.stop()
-        muxer.release()
+                encode(surface, bufferInfo, codec, muxer, size)
+                // Release resources
+                codec.stop()
+                codec.release()
+                muxer.stop()
+                muxer.release()
+            } catch (e: Exception) {
+                Log.e(TAG, "Error: ${e.message}")
+            } finally {
+                _state.postValue(EncodingState.Complete)
+            }
+        }
     }
 
     private fun renderBitmap(drawable: Int, surface: Surface, size: Int) {
@@ -74,7 +96,7 @@ class AnimationViewModel : ViewModel() {
         muxer: MediaMuxer,
         size: Int
     ) {
-        val DEQUEUE_TIMEOUT_USEC = 10000L
+        val dequeueTimeoutUsec = 10000L
         val videoDuration = 5 * 1000000L
         var startTime: Long = -1
         var isEndOfStream = false
@@ -82,7 +104,7 @@ class AnimationViewModel : ViewModel() {
         var index = 0
         while (true) {
             renderBitmap(drawableList[index], surface, size)
-            val outputBufferIndex = encoder.dequeueOutputBuffer(mBufferInfo, DEQUEUE_TIMEOUT_USEC)
+            val outputBufferIndex = encoder.dequeueOutputBuffer(mBufferInfo, dequeueTimeoutUsec)
             val presentationTimeUs = mBufferInfo.presentationTimeUs
             if (startTime == -1L && presentationTimeUs > 0) {
                 startTime = presentationTimeUs
@@ -95,20 +117,20 @@ class AnimationViewModel : ViewModel() {
                 }
             }
             if (outputBufferIndex == MediaCodec.INFO_TRY_AGAIN_LATER) {
-                Log.i("Tim", "no output available, spinning to await EOS")
+                Log.i(TAG, "no output available, spinning to await EOS")
                 //break
             } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                Log.i("Tim", "start muxer")
+                Log.i(TAG, "start muxer")
                 videoTrackIndex = muxer.addTrack(encoder.outputFormat)
                 muxer.start()
             } else if (outputBufferIndex < 0) {
-                Log.i("Tim", "unexpected result from encoder.dequeueOutputBuffer")
+                Log.i(TAG, "unexpected result from encoder.dequeueOutputBuffer")
             } else {
                 val encodedDataBuffer = encoder.getOutputBuffer(outputBufferIndex)
                     ?: throw RuntimeException("encoderOutputBuffer $outputBufferIndex was null")
 
                 if (mBufferInfo.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG != 0) {
-                    Log.i("Tim", "ignoring BUFFER_FLAG_CODEC_CONFIG")
+                    Log.i(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG")
                     mBufferInfo.size = 0
                 }
 
@@ -122,7 +144,7 @@ class AnimationViewModel : ViewModel() {
                 encoder.releaseOutputBuffer(outputBufferIndex, false)
 
                 if (mBufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
-                    Log.i("Tim", "get EOS")
+                    Log.i(TAG, "Get EOS")
                     break
                 }
             }
@@ -133,7 +155,7 @@ class AnimationViewModel : ViewModel() {
 
         callback?.let {
             val file = it.getFile()
-            Log.i("Tim", "file: ${file.absolutePath}")
+            Log.i(TAG, "file: ${file.absolutePath}")
             if (file.exists()) {
                 file.delete()
             }
